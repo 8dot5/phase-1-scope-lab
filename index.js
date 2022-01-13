65f86510a5ef746ae9edc1172e7687aa73513f98
Chun Yi <8.5by11@gmail.com>
2022-01-12 19:12:26 -0500

    commit tree 4495dbbacd8f8275c21efc3ea030c85b6a4bbb09

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'carl';
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+    return leastFavoriteCustomer;
+}
